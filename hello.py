9193c04bac6b42e4c4d2af40e36cc5aeb4ea688e
bryson705 <bkh6305@uncw.edu>
2022-08-25 15:02:22 -0400

    my first commit

---
+++ hello.py
@@ -0,0 +1,14 @@
+# I wrote some things.
+# Bryson Harllee
+
+def print_hello(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print("yes")
+    print("12345")
+    print("Hello World!")
+
+if __name__ == '__main__':
+    print_hello('Bryson')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
